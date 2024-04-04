de73299b827d3fe45a72b93ef87e8112c07ff9ac
Otacilio Neto <otacilio.neto@tjdft.jus.br>
2024-04-04 17:09:36 -0300

    primeiro commit

---
+++ src/main/java/br/cnj/primeiroexemplo/PrimeiroexemploApplication.java
@@ -0,0 +1,13 @@
+package br.cnj.primeiroexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroexemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroexemploApplication.class, args);
+	}
+
+}
